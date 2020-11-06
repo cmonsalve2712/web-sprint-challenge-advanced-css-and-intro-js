a442330b147e47ca4561894ec22586bc71766422
cmonsalve2712 <cmonsalve2712@gmail.com>
2020-11-06 16:33:33 -0500

    still working on it

--- index.js
+++ index.js
@@ -209,10 +209,15 @@ Practice accessing data above by console.log-ing following items:
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
-
+console.log(Artists.name[0]);
+console.log(Artists.bio[2]);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+
+console.log("Before update: ", artists[8])
+artists[8].name = "Vincent Van Gogh"
+console.log("After update: ", artists[8])
 
 
 
@@ -225,8 +230,11 @@ Create a function called `getArtistByIndex` that takes two arguments:
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array, index) {
+  /* code here */
+  if (array[index]) {
+    return `The artist at index ${index} is ${array[index].name}`;
+  }
 }
 
   
@@ -234,9 +242,17 @@ function getArtistByIndex(/*Your Code Here*/) {
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array){
+  const twentiesArtists = []
+  for(let i = 0; i < array.length; i++){
+    if(parseInt(array[i].years) > 1899){
+      twentiesArtists.push(array[i].name);
+    }
+  }
+  return twentiesArtists;
 }
+
+console.log(get20s(artists));
 
 
 
@@ -251,9 +267,13 @@ Create a function called `removeArtist` that takes two arguments:
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*Your Code Here*/) {
+function removeArtist(array, index){
   /*Your Code Here*/
+  array.splice(index, 1);
+
+  return array;
 }
+
    
 
 /**
